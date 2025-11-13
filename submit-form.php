f304a50b365105675cce4c55ec12feb36e3af920
Trevor J. DeWall <32908114+td2334@users.noreply.github.com>
2025-11-12 22:37:03 -0500

    Delete submit-form.php

--- submit-form.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Ensure the request method is POST
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Include the actual form processing script
-    include '../php/process-form.php';
-} else {
-    // Handle invalid request methods
-    echo "Invalid request method.";
-}
